ff31a22a09de02e4b482bf138e3234a1bd1a2630
Isaac <a1901982@student.adelaide.edu.au>
2026-09-05 21:47:44 +0930

    adding more tests

--- RegexEngine_Test.java
+++ RegexEngine_Test.java
@@ -63,4 +63,164 @@ public class RegexEngine_Test {
         assertFalse(RegexEngine.matches(nfa, "b"));
         assertFalse(RegexEngine.matches(nfa, "ab"));
     }
+
+    @Test
+    public void testParentheses() {
+        RegexEngine.nextStateId = 0;
+
+        RegexEngine.NFA nfa = RegexEngine.buildBasicNFA("(ab)");
+
+        assertTrue(RegexEngine.matches(nfa, "ab"));
+        assertFalse(RegexEngine.matches(nfa, "a"));
+        assertFalse(RegexEngine.matches(nfa, "b"));
+        assertFalse(RegexEngine.matches(nfa, "abc"));
+    }
+
+    @Test
+    public void testParenthesesWithStar() {
+        RegexEngine.nextStateId = 0;
+
+        RegexEngine.NFA nfa = RegexEngine.buildBasicNFA("(ab)*");
+
+        assertTrue(RegexEngine.matches(nfa, ""));
+        assertTrue(RegexEngine.matches(nfa, "ab"));
+        assertTrue(RegexEngine.matches(nfa, "abab"));
+        assertTrue(RegexEngine.matches(nfa, "ababab"));
+
+        assertFalse(RegexEngine.matches(nfa, "a"));
+        assertFalse(RegexEngine.matches(nfa, "b"));
+        assertFalse(RegexEngine.matches(nfa, "aba"));
+    }
+
+    @Test
+    public void testParenthesesWithPlus() {
+        RegexEngine.nextStateId = 0;
+
+        RegexEngine.NFA nfa = RegexEngine.buildBasicNFA("(ab)+");
+
+        assertFalse(RegexEngine.matches(nfa, ""));
+        assertTrue(RegexEngine.matches(nfa, "ab"));
+        assertTrue(RegexEngine.matches(nfa, "abab"));
+        assertTrue(RegexEngine.matches(nfa, "ababab"));
+
+        assertFalse(RegexEngine.matches(nfa, "a"));
+        assertFalse(RegexEngine.matches(nfa, "aba"));
+    }
+
+    @Test
+    public void testAlternationWithConcatenation() {
+        RegexEngine.nextStateId = 0;
+
+        RegexEngine.NFA nfa = RegexEngine.buildBasicNFA("a|bc");
+
+        assertTrue(RegexEngine.matches(nfa, "a"));
+        assertTrue(RegexEngine.matches(nfa, "bc"));
+
+        assertFalse(RegexEngine.matches(nfa, "b"));
+        assertFalse(RegexEngine.matches(nfa, "abc"));
+        assertFalse(RegexEngine.matches(nfa, "ac"));
+    }
+
+    @Test
+    public void testConcatenationWithAlternation() {
+        RegexEngine.nextStateId = 0;
+
+        RegexEngine.NFA nfa = RegexEngine.buildBasicNFA("(a|b)c");
+
+        assertTrue(RegexEngine.matches(nfa, "ac"));
+        assertTrue(RegexEngine.matches(nfa, "bc"));
+
+        assertFalse(RegexEngine.matches(nfa, "a"));
+        assertFalse(RegexEngine.matches(nfa, "b"));
+        assertFalse(RegexEngine.matches(nfa, "abc"));
+    }
+
+    @Test
+    public void testAssignmentExample() {
+        RegexEngine.nextStateId = 0;
+
+        RegexEngine.NFA nfa = RegexEngine.buildBasicNFA("(ab)*|c+");
+
+        assertTrue(RegexEngine.matches(nfa, ""));
+        assertTrue(RegexEngine.matches(nfa, "ab"));
+        assertTrue(RegexEngine.matches(nfa, "abab"));
+        assertTrue(RegexEngine.matches(nfa, "c"));
+        assertTrue(RegexEngine.matches(nfa, "cc"));
+        assertTrue(RegexEngine.matches(nfa, "ccc"));
+
+        assertFalse(RegexEngine.matches(nfa, "a"));
+        assertFalse(RegexEngine.matches(nfa, "b"));
+        assertFalse(RegexEngine.matches(nfa, "abc"));
+        assertFalse(RegexEngine.matches(nfa, "ac"));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testEmptyRegex() {
+        RegexEngine.buildBasicNFA("");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testNullRegex() {
+        RegexEngine.buildBasicNFA(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testUnexpectedClosingParenthesis() {
+        RegexEngine.buildBasicNFA("a)");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testMissingClosingParenthesis() {
+        RegexEngine.buildBasicNFA("(ab");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testUnexpectedStar() {
+        RegexEngine.buildBasicNFA("*a");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testUnexpectedPlus() {
+        RegexEngine.buildBasicNFA("+a");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testAlternationAtStart() {
+        RegexEngine.buildBasicNFA("|a");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testAlternationAtEnd() {
+        RegexEngine.buildBasicNFA("a|");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testEmptyParentheses() {
+        RegexEngine.buildBasicNFA("()");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testOperatorAfterOpeningParenthesis() {
+        RegexEngine.buildBasicNFA("(*a)");
+    }
+
+    
+    @Test
+    public void testUppercaseLetters() {
+        RegexEngine.nextStateId = 0;
+    
+        RegexEngine.NFA nfa = RegexEngine.buildBasicNFA("ABC");
+        assertTrue(RegexEngine.matches(nfa, "ABC"));
+        assertFalse(RegexEngine.matches(nfa, "abc"));
+    }
+
+    @Test
+    public void testNumbers() {
+        RegexEngine.nextStateId = 0;
+        
+        RegexEngine.NFA nfa = RegexEngine.buildBasicNFA("123");
+        assertTrue(RegexEngine.matches(nfa, "123"));
+        assertFalse(RegexEngine.matches(nfa, "12"));
+        assertFalse(RegexEngine.matches(nfa, "1234"));
+    }
 }
